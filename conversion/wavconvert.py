495c3e2ffee7efc59fa604d736c14439dd29fd12
thomas-kowalski <thomaskowalski@outlook.fr>
2022-10-05 17:25:11 +0200

    add auto rename flag

--- conversion/wavconvert.py
+++ conversion/wavconvert.py
@@ -1,4 +1,5 @@
 #%% flac to wav conversion. all files needs to have .flac extension
+# use auto_rename flag to rename wav > flac automatically
 # NOT ROBUST for mixed wav/flac folders. using soundfile incapability of reading flac files for format detection.
 
 import os
@@ -13,7 +14,14 @@ def flac_browse(folder):
             matches.append(os.path.join(root, f))
     return matches
 
-def main(root_samples, remove=True):
+def main(root_samples, auto_rename=True, remove=True):
+    if auto_rename:
+        for root, _, files in os.walk(root_samples):
+            for f in files:
+                if not f.endswith(".wav") or "._" in f: continue
+                path = os.path.join(root, f)
+                os.rename(path, path.replace(".wav", ".flac"))
+
     for sample in flac_browse(root_samples):
         iswav = False
         try:
