277a806aa9788ef1910b406cf16497e15ca22bad
thomas-kowalski <thomaskowalski@outlook.fr>
2022-12-12 17:03:54 +0100

    fix arg error

--- dsp/pitchshift.py
+++ dsp/pitchshift.py
@@ -8,15 +8,15 @@ def pitch_shift(x, semitones):
     ratio = 2 ** (semitones * -1 / 12)
     return samplerate.resample(x, ratio, "sinc_best")
 
-def main(root_samples):
+def main(root_samples, semitone_offset):
     for sample in sf.browse(root_samples):
         x, sr = sf.read(sample)
-        xshift = pitch_shift(x, 12)
+        xshift = pitch_shift(x, semitone_offset)
         sf.write(sample, xshift , sr, subtype=sf.info(sample).subtype)
 
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) == 2:
-        main(sys.argv[1])
+    if len(sys.argv) == 3:
+        main(sys.argv[1], sys.argv[2])
     else:
-        print("usage: python <script> <semitones offset>")
+        print("usage: python <script> <samples folder> <semitones offset>")
